a61066d8dd1b6c2c7b5e23857f53e1398d02102e
Uğur <uggurkurt@gmail.com>
2016-11-01 09:44:25 +0000

    Finished Product

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require("express");
 var app = express();
 
 app.get('/',function(req,res){
-    
+    var sendMe = { ip:req.headers['x-forwarded-for'] , language:req.headers['accept-language'] ,system:req.headers['user-agent']};
+    res.end(JSON.stringify(sendMe));
 });
 
 
